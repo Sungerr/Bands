9818fb59089319afb0d57fe9c9c2495da7a3dbee
Wersdgs <682935@sylvaniastudents.org>
2022-10-08 12:45:47 -0400

    Fixed Activities

--- app/src/main/java/com/example/bands/MainActivity.java
+++ app/src/main/java/com/example/bands/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.bands;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button statsButton, trackButton, scanButton, travelButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        statsButton = findViewById(R.id.statsButton);
+        statsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent statsIntent = new Intent(getApplicationContext(), ScanActivity.class);
+                startActivity(statsIntent);
+            }
+        });
+
+        trackButton = findViewById(R.id.trackButton);
+        trackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent statsIntent = new Intent(getApplicationContext(), TrackActivity.class);
+                startActivity(statsIntent);
+            }
+        });
+
+        scanButton = findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent scanIntent = new Intent(getApplicationContext(), ScanActivity.class);
+                startActivity(scanIntent);
+            }
+        });
+
+        travelButton = findViewById(R.id.travelButton);
+        travelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent travelIntent = new Intent(getApplicationContext(), TravelActivity.class);
+                startActivity(travelIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bands/ScanActivity.java
+++ app/src/main/java/com/example/bands/ScanActivity.java
@@ -1,8 +1,8 @@
 package com.example.bands;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class ScanActivity extends AppCompatActivity {
 
