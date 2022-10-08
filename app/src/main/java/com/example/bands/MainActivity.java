76999b434a62c7a93d21b277e78319066991b925
Wersdgs <682935@sylvaniastudents.org>
2022-10-08 11:39:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bands/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bands;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
