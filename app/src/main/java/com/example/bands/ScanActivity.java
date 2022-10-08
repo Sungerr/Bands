61029b6c622b0a0f5fb717fb18c41b1e8500347e
Wersdgs <682935@sylvaniastudents.org>
2022-10-08 18:37:22 -0400

    NFC Reader

--- app/src/main/java/com/example/bands/ScanActivity.java
+++ app/src/main/java/com/example/bands/ScanActivity.java
@@ -6,9 +6,13 @@ import androidx.constraintlayout.widget.Placeholder;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -42,35 +46,63 @@ public class ScanActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        Intent intent = new Intent(this, ScanActivity.class);
-        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        if (nfcAdapter != null && nfcAdapter.isEnabled()) {
+            Intent intent = new Intent(this, ScanActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        IntentFilter[] intentFilter = new IntentFilter[]{};
-        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+            IntentFilter[] intentFilter = new IntentFilter[]{};
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilter, null);
+        }
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        nfcAdapter.disableForegroundDispatch(this);
+        if (nfcAdapter != null && nfcAdapter.isEnabled()) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
         super.onPause();
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
+        if (intent.hasExtra(NfcAdapter.EXTRA_TAG)) {
+            //Toast.makeText(this, "NFC Intent!", Toast.LENGTH_LONG).show();
+
+            Parcelable[] parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (parcelables != null && parcelables.length > 0) {
+                getTagInfo((NdefMessage) parcelables[0]);
+            } else {
+                Toast.makeText(this, "No NDEF messages!", Toast.LENGTH_SHORT).show();
+            }
+        }
         super.onNewIntent(intent);
-        Toast.makeText(this, "NFC Intent Received!", Toast.LENGTH_LONG).show();
     }
-//
-//    private void getTagInfo(Intent intent) {
-//        String action = intent.getAction();
-//
-//        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)|| NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-//                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-//            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-//
-//        }
-//
-//    }
+
+    private void getTagInfo(NdefMessage ndefMessage) {
+        NdefRecord[] ndefRecords = ndefMessage.getRecords();
+
+        if (ndefRecords != null && ndefRecords.length > 0) {
+            NdefRecord ndefRecord = ndefRecords[0];
+
+            String tagContent = getNdefInfo(ndefRecord);
+            textViewInfo.setText(tagContent);
+        } else {
+            Toast.makeText(this, "No NDEF Records Found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getNdefInfo(NdefRecord ndefRecord) {
+        String tagContent = null;
+        try {
+            byte[] payload = ndefRecord.getPayload();
+            String encoding = ((payload[0] & 128) == 0) ? "UTF-8" :"UTF-16";
+            int languageSize = payload[0] & 0063;
+            tagContent = new String(payload, languageSize+1,payload.length-languageSize-1, encoding);
+        } catch (Exception e) {
+            Log.e("getTextFromNdeRecord", e.getMessage(), e);
+        }
+        return tagContent;
+    }
 }
