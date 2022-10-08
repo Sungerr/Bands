7d0fa9708ad127e9c3c7c74c9a3440ebcc62eb06
Wersdgs <682935@sylvaniastudents.org>
2022-10-08 12:07:38 -0400

    Added activity onCreate methods

--- app/src/main/java/com/example/bands/TrackActivity.java
+++ app/src/main/java/com/example/bands/TrackActivity.java
@@ -1,4 +1,14 @@
 package com.example.bands;
 
-public class TrackActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TrackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track);
+    }
 }
--- app/src/main/java/com/example/bands/TravelActivity.java
+++ app/src/main/java/com/example/bands/TravelActivity.java
@@ -1,4 +1,14 @@
 package com.example.bands;
 
-public class TravelActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TravelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel);
+    }
 }
